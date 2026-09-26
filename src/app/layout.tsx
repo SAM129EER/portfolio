b55cd8ba0b22686011f96ee23f6a0748e4223c41
Sameer <jangidsameer77@gmail.com>
2026-09-26 22:06:27 +0530

    changed layout and added footer and header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Container from "@/components/Container";
+import Footer from "@/components/Footer";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -18,10 +19,12 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={`${inter.className} h-full antialiased`}>
       <body className="flex min-h-full flex-col bg-neutral-100 dark:bg-neutral-800">
+        
         <Container className="flex min-h-full flex-col">
           <Navbar />
-          <main className="flex-1 p-4 md:p-10">{children}</main>
+          <main className="min-h-screen p-4 md:p-10">{children}</main>
         </Container>
+       
       </body>
     </html>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,9 +11,9 @@ const Navbar = () => {
   return (
     <nav className="flex items-center justify-between px-10 py-2">
       <div className="items-center rounded border border-neutral-300 px-3 py-1.5">
-        <Link href={"/"}>Sameer</Link>
+        <Link href={"/"}>Sameer Jangid</Link>
       </div>
-      <div className="flex items-center gap-2 p-2">
+      <div className="flex items-center border border-neutral-300 rounded gap-3 p-2 ">
         {navItems.map((item) => (
           <Link key={item.id} href={item.href}>
             {item.title}
